aeab4d5880408251505174aa4f9ae413804962c0
Martin Podloucký <podlouckymartin@gmail.com>
2022-04-10 14:41:42 +0200

    Base URL pro nasazení na server

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@ import express from 'express';
 import { moviesRouter } from './apis/movies/index.js';
 
 const port = process.env.PORT || 4000;
+const baseUrl = process.env.BASE_URL || '';
 
 const app = express();
 
-// app.use('/', (req, res) => {
-//   res.redirect(301, '/docs');
-// });
-
-app.use('/docs', express.static('docs/_site', {
+app.use(`${baseUrl}/docs`, express.static('docs/_site', {
   extensions: ['html'],
 }));
 
-app.use('/apis/movies', moviesRouter);
+app.use(`${baseUrl}/apis/movies`, moviesRouter);
+
+app.use(`${baseUrl}/`, (req, res) => {
+  res.redirect(`${baseUrl}/docs`);
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
